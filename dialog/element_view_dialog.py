17b72b71dfe8b7f995720b9eef2a7172d743860b
Morita-T1700 <penelope.hide.morita@gmail.com>
2021-08-23 17:23:50 +0900

    要素テーブルを呼び出すダイアログを追加

---
+++ dialog/element_view_dialog.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 23 17:16:17 2021
+
+@author: Morita-T1700
+"""
+
+from PyQt5.QtWidgets import QApplication, QDialog
+
+class ElementsViewDialog(QDialog):
+    
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        
+        
